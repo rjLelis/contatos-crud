c83f12b2b51fa13c3fc30f41af0f86d16021b0b7
rjLelis <renatojlelis@gmail.com>
2019-08-03 12:39:38 -0300

    Change main url config

--- contatos_base/urls.py
+++ contatos_base/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contatos/', include('contatos.urls', namespace='contatos'))
+    path('', include('contatos.urls', namespace='contatos'))
 ]
